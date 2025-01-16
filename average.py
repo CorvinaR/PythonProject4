cf662ad9fd4d381c9602b1fc836e6a4b43139e79
Rayna Corvina <CorvinaR@oregonstate.edu>
2025-01-15 22:33:13 -0800

    Initial commit

---
+++ average.py
@@ -0,0 +1,14 @@
+# Author: Rayna Corvina
+# GitHub username: corvinar
+# Date: 1/15/2024
+# Description: Finds the average of five input numbers.
+print("Please enter five numbers.")
+num_1 = float(input())
+num_2 = float(input())
+num_3 = float(input())
+num_4 = float(input())
+num_5 = float(input())
+#Begin Calculations & Output
+average_num =  (num_1 + num_2 + num_3 + num_4 + num_5)/5
+print('The average of those numbers is:')
+print(average_num)
